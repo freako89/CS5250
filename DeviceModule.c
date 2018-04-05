a63d222f4f25ee7b3625efc7f0fd449e95bf9c23
freako89 <alanchee_89@msn.com>
2018-04-05 22:33:42 +0800

    Changed return error for onebyte_write from EINVAL to ENOSPC

--- DeviceModule.c
+++ DeviceModule.c
@@ -6,22 +6,24 @@
 #include <linux/types.h>
 #include <linux/fs.h>
 #include <linux/proc_fs.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
+
 #define MAJOR_NUMBER 61/* forward declaration */
+
 int onebyte_open(struct inode *inode, struct file *filep);
 int onebyte_release(struct inode *inode, struct file *filep);
-ssize_t onebyte_read(struct file *filep, char *buf, size_t
-		count, loff_t *f_pos);
-ssize_t onebyte_write(struct file *filep, const char *buf,
-		size_t count, loff_t *f_pos);
+ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos);
+ssize_t onebyte_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos);
 static void onebyte_exit(void);
+
 /* definition of file_operation structure */
 struct file_operations onebyte_fops = {
 	read: onebyte_read,
-      	write: onebyte_write,
-      	open: onebyte_open,
-      	release: onebyte_release
+	write: onebyte_write,
+	open: onebyte_open,
+	release: onebyte_release
 };
+
 char *onebyte_data = NULL;
 int onebyte_open(struct inode *inode, struct file *filep)
 {
@@ -35,13 +37,13 @@ ssize_t onebyte_read(struct file *filep, char *buf, size_t
 		count, loff_t *f_pos)
 {
 	/*please complete the function on your own*/
-	int byte_read = 0;
+	int bytes_read = 0;
 	
 	/* Check if buffer has been written*/
 	if (*buf!=0)
 		return 0;
 
-	copy_to_user(buf, onebyte_data, sizeof(char));
+	raw_copy_to_user(buf, onebyte_data, sizeof(char));
 	
 	bytes_read++;
 	return bytes_read;
@@ -50,8 +52,8 @@ ssize_t onebyte_write(struct file *filep, const char *buf,
 		size_t count, loff_t *f_pos)
 {
 	/*please complete the function on your own*/
-	int byte_write = 0;
-	copy_from_user(onebyte_data, buf, sizeof(char));
+	int bytes_write = 0;
+	raw_copy_from_user(onebyte_data, buf, sizeof(char));
 	
 	/* Check length of the bytes that have been written*/
 	if(count > sizeof(char))
@@ -66,9 +68,8 @@ ssize_t onebyte_write(struct file *filep, const char *buf,
 static int onebyte_init(void)
 {
 	int result;
-	register the device
-		result = register_chrdev(MAJOR_NUMBER, "onebyte",
-				&onebyte_fops);
+	//register the device
+	result = register_chrdev(MAJOR_NUMBER, "onebyte", &onebyte_fops);
 	if (result < 0) {
 		return result;
 	}
